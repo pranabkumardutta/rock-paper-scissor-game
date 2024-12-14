351ea43085fa150900c154b9d7d463cba16dbe32
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 14:25:07 +0530

    add images

--- rockpaper.py
+++ rockpaper.py
@@ -6,6 +6,25 @@ window = Tk()
 window.title("Game Rock Paper and  Scissor")
 window.configure(background="black")
 
+image_rocks1= ImageTk.PhotoImage(Image.open("rock1.png"))
+image_paper1= ImageTk.PhotoImage(Image.open("paper1.png"))
+image_scissor1= ImageTk.PhotoImage(Image.open("scissor1.png"))
+image_rocks2= ImageTk.PhotoImage(Image.open("rock2.png"))
+image_paper2= ImageTk.PhotoImage(Image.open("paper2.png"))
+image_scissor2= ImageTk.PhotoImage(Image.open("scissor2.png"))
+
+
+label_player = Label(window,image=image_scissor1)
+label_computer = Label(window,image=image_scissor2)
+label_computer.grid(row=1,column=0)
+label_player.grid(row=1 , column=4)
+
+
+
+ 
+
+
+
 button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=1)
 
 
