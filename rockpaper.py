0aa88bf5d8de27656d4fc305cae356b3fa0d70d7
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 15:46:18 +0530

    condition to cheack winner

--- rockpaper.py
+++ rockpaper.py
@@ -37,16 +37,55 @@ computer_indicator = Label(window,font=("arial",40,"bold"),
 computer_indicator.grid(row=0 , column=1)
 player_indicator.grid(row=0 , column=3)
 
-
+#message for who win or who loss
 final_message = Label(window,font=("arial",40,"bold"),bg="red",fg="white")
 final_message.grid(row=3 ,column=2)
 
+def msg_updation(a):
+    final_message['text'] = a
+
+def Computer_Update():
+    final = int(score_computer['text'])
+    final += 1
+    score_computer["text"] = str(final)
+
+def Player_Update():
+    final = int(score_player['text'])
+    final += 1
+    score_player["text"] = str(final)
+
+def winner_check(p,c):
+    if p ==c:
+        updateMessage("It's a tie ")
+    elif p == "rock":
+        if c == "paper":
+            updateMessage("Computer Wins.")
+            Computer_Update()
+        else:
+            updateMessage("player Wins.")
+            Player_Update()
+
+    elif p == "paper":
+       if c == "scissor":
+           updateMessage("Computer Wins")
+           Computer_Update()
+       else:
+             updateMessage("player Wins.")
+             Player_Update()
+    elif p == "scissor":
+        if  c == "rock":
+            updateMessage("Computer Wins")
+            Computer_Update()
+        else:
+            updateMessage("player Wins.")
+            Player_Update()
+
+    else:
+        pass
+
+#button
 button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=1)
-
-
 button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=2)
-
-
 button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=3)
 
 
