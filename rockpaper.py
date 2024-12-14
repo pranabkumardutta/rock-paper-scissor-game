3c6619a7f310ec4b71bb3e647a10b59580e39338
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 16:11:18 +0530

    show image as to choce

--- rockpaper.py
+++ rockpaper.py
@@ -41,19 +41,20 @@ player_indicator.grid(row=0 , column=3)
 final_message = Label(window,font=("arial",40,"bold"),bg="red",fg="white")
 final_message.grid(row=3 ,column=2)
 
-def msg_updation(a):
+def updateMessage(a):
     final_message['text'] = a
 
 def Computer_Update():
-    final = int(score_computer['text'])
+    final = int(computer_score['text'])
     final += 1
-    score_computer["text"] = str(final)
+    computer_score["text"] = str(final)
 
 def Player_Update():
-    final = int(score_player['text'])
+    final = int(player_score['text'])
     final += 1
-    score_player["text"] = str(final)
+    player_score["text"] = str(final)
 
+#condition to cheack winner
 def winner_check(p,c):
     if p ==c:
         updateMessage("It's a tie ")
@@ -83,10 +84,34 @@ def winner_check(p,c):
     else:
         pass
 
+
+to_select = ["rock" ,"paper" ,"scissor"]
+
+def choice_update(a):
+#show image as to the choice
+    choice_computer = to_select[randint(0,2)]
+#for computer
+    if choice_computer == "rock":
+        label_computer.configure(image=image_rocks2)
+    elif choice_computer == "paper":
+        label_computer.configure(image = image_paper2)
+    else :
+        label_computer.configure(image = image_scissor2)
+#for player
+    if a == "rock":
+        label_player.configure(image =image_rocks1)
+    elif a == "paper":
+        label_player.configure(image =image_paper1)
+    else: 
+        label_player.configure(image =image_scissor1)
+
+    winner_check(a , choice_computer)
+
+
 #button
-button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=1)
-button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=2)
-button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=3)
+button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red", command=lambda:choice_update("rock"),).grid(row=4 , column=1)
+button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",command=lambda:choice_update("paper"),).grid(row=4 , column=2)
+button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",command=lambda:choice_update("scissor"),).grid(row=4 , column=3)
 
 
 
