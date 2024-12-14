4490e1fa3eff48e2b0889332ee988659e14ecdc7
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 15:19:50 +0530

    lable for the computer and player

--- rockpaper.py
+++ rockpaper.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from PIL import Image , ImageTk
 from random import  randint
@@ -6,32 +7,44 @@ window = Tk()
 window.title("Game Rock Paper and  Scissor")
 window.configure(background="black")
 
-image_rocks1= ImageTk.PhotoImage(Image.open("rock1.png"))
-image_paper1= ImageTk.PhotoImage(Image.open("paper1.png"))
-image_scissor1= ImageTk.PhotoImage(Image.open("scissor1.png"))
-image_rocks2= ImageTk.PhotoImage(Image.open("rock2.png"))
-image_paper2= ImageTk.PhotoImage(Image.open("paper2.png"))
-image_scissor2= ImageTk.PhotoImage(Image.open("scissor2.png"))
+image_rocks1 = ImageTk.PhotoImage(Image.open("rock1.png").resize((200, 196)))
+image_paper1 = ImageTk.PhotoImage(Image.open("paper1.png").resize((200, 196)))
+image_scissor1 = ImageTk.PhotoImage(Image.open("scissor1.png").resize((200, 196)))
+image_rocks2 = ImageTk.PhotoImage(Image.open("rock2.png").resize((200, 196)))
+image_paper2 = ImageTk.PhotoImage(Image.open("paper2.png").resize((200, 196)))
+image_scissor2 = ImageTk.PhotoImage(Image.open("scissor2.png").resize((200, 196)))
 
 
-label_player = Label(window,image=image_scissor1)
-label_computer = Label(window,image=image_scissor2)
-label_computer.grid(row=1,column=0)
-label_player.grid(row=1 , column=4)
+label_player = Label(window,image=image_scissor2)
+label_computer = Label(window,image=image_scissor1)
+label_computer.grid(row=2,column=0)
+label_player.grid(row=2, column=4)
+
+
+computer_score= Label(window,font=('arial',60,"bold"),
+                      text="0",fg="red")
+player_score = Label(window,font=('arial',60,"bold"),
+                     text="0",fg="red")
+computer_score.grid(row=2 , column=1)
+player_score.grid(row=2 , column=3)
+
+player_indicator = Label(window,font=("arial",40,"bold"),
+                         text="PLAYER", bg="orange",fg="blue")
+
+computer_indicator = Label(window,font=("arial",40,"bold"),
+                         text="COMPUTER", bg="orange",fg="blue")
+computer_indicator.grid(row=0 , column=1)
+player_indicator.grid(row=0 , column=3)
 
 
 
- 
+button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=1)
 
 
-
-button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=1)
-
-
-button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=2)
+button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=2)
 
 
-button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=3)
+button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=4 , column=3)
 
 
 
