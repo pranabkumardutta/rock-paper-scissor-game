348830565586d8c45a147a891e68c6174f1301cb
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 13:29:28 +0530

    add 3 buttom for rock ,paper , scissor

--- rockpaper.py
+++ rockpaper.py
@@ -6,4 +6,14 @@ window = Tk()
 window.title("Game Rock Paper and  Scissor")
 window.configure(background="black")
 
+button_rock = Button(window,width=16 , height=3 , text="ROCK", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=1)
+
+
+button_paper = Button(window,width=16 , height=3 , text="PAPER", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=2)
+
+
+button_scissor = Button(window,width=16 , height=3 , text="SCISSOR", font=("arial",20,"bold"),bg='yellow',fg="red",).grid(row=2 , column=3)
+
+
+
 window.mainloop()
