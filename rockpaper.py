c43ee4afa69f866b0c012dd425d8188c1dc0c6dd
pranabkumardutta <142892393+pranabkumardutta@users.noreply.github.com>
2024-12-14 13:22:06 +0530

    just create a tkinter interface for prj

--- rockpaper.py
+++ rockpaper.py
@@ -3,4 +3,7 @@ from PIL import Image , ImageTk
 from random import  randint
 
 window = Tk()
-window.title
+window.title("Game Rock Paper and  Scissor")
+window.configure(background="black")
+
+window.mainloop()
